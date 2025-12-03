8a359a592808399ee6c14ac037f1e497f094af93
your name <phuonghuynh10062005@gmail.com>
2025-12-03 13:13:10 +0700

    baitap

---
+++ BTVNbuoi4/BTVNbuoi4/Form1.Designer.cs
@@ -0,0 +1,366 @@
+﻿namespace BTVNbuoi4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBoxKhoa = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtDiemTB = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.radioNu = new System.Windows.Forms.RadioButton();
+            this.radioNam = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtHoTen = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMaSV = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblTongNam = new System.Windows.Forms.Label();
+            this.lblTongNu = new System.Windows.Forms.Label();
+            this.MSSV = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.HoTen = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.GioiTinh = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Khoa = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DiemTB = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.MSSV,
+            this.HoTen,
+            this.GioiTinh,
+            this.Khoa,
+            this.DiemTB});
+            this.dataGridView1.Location = new System.Drawing.Point(319, 112);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(543, 241);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 6);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(865, 68);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Quản Lí Thông Tin Sinh Viên";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBoxKhoa);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txtDiemTB);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.radioNu);
+            this.groupBox1.Controls.Add(this.radioNam);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtHoTen);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtMaSV);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(14, 98);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(283, 268);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông Tin Sinh Viên";
+            // 
+            // comboBoxKhoa
+            // 
+            this.comboBoxKhoa.FormattingEnabled = true;
+            this.comboBoxKhoa.Items.AddRange(new object[] {
+            "QTKD",
+            "CNTT",
+            "NNA"});
+            this.comboBoxKhoa.Location = new System.Drawing.Point(93, 162);
+            this.comboBoxKhoa.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.comboBoxKhoa.Name = "comboBoxKhoa";
+            this.comboBoxKhoa.Size = new System.Drawing.Size(82, 21);
+            this.comboBoxKhoa.TabIndex = 13;
+            this.comboBoxKhoa.Text = "QTKD";
+            this.comboBoxKhoa.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(149, 212);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(89, 21);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Xóa";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 212);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 21);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Thêm/Sữa";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(11, 162);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Chuyên Ngành";
+            // 
+            // txtDiemTB
+            // 
+            this.txtDiemTB.Location = new System.Drawing.Point(93, 126);
+            this.txtDiemTB.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtDiemTB.Name = "txtDiemTB";
+            this.txtDiemTB.Size = new System.Drawing.Size(88, 20);
+            this.txtDiemTB.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(11, 126);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Điểm TB";
+            // 
+            // radioNu
+            // 
+            this.radioNu.AutoSize = true;
+            this.radioNu.Location = new System.Drawing.Point(160, 96);
+            this.radioNu.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.radioNu.Name = "radioNu";
+            this.radioNu.Size = new System.Drawing.Size(39, 17);
+            this.radioNu.TabIndex = 6;
+            this.radioNu.TabStop = true;
+            this.radioNu.Text = "Nữ";
+            this.radioNu.UseVisualStyleBackColor = true;
+            this.radioNu.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioNam
+            // 
+            this.radioNam.AutoSize = true;
+            this.radioNam.Location = new System.Drawing.Point(93, 96);
+            this.radioNam.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.radioNam.Name = "radioNam";
+            this.radioNam.Size = new System.Drawing.Size(47, 17);
+            this.radioNam.TabIndex = 5;
+            this.radioNam.TabStop = true;
+            this.radioNam.Text = "Nam";
+            this.radioNam.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 96);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Giới Tính";
+            // 
+            // txtHoTen
+            // 
+            this.txtHoTen.Location = new System.Drawing.Point(93, 62);
+            this.txtHoTen.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtHoTen.Name = "txtHoTen";
+            this.txtHoTen.Size = new System.Drawing.Size(187, 20);
+            this.txtHoTen.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 62);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Họ Tên";
+            // 
+            // txtMaSV
+            // 
+            this.txtMaSV.Location = new System.Drawing.Point(93, 26);
+            this.txtMaSV.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtMaSV.Name = "txtMaSV";
+            this.txtMaSV.Size = new System.Drawing.Size(165, 20);
+            this.txtMaSV.TabIndex = 1;
+            this.txtMaSV.TextChanged += new System.EventHandler(this.txtMaSV_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 26);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Mã Sinh Viên";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(561, 369);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(105, 13);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "Tổng Sinh Viên Nam";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(757, 369);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(21, 13);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "Nữ";
+            // 
+            // lblTongNam
+            // 
+            this.lblTongNam.AutoSize = true;
+            this.lblTongNam.Location = new System.Drawing.Point(686, 369);
+            this.lblTongNam.Name = "lblTongNam";
+            this.lblTongNam.Size = new System.Drawing.Size(0, 13);
+            this.lblTongNam.TabIndex = 5;
+            // 
+            // lblTongNu
+            // 
+            this.lblTongNu.AutoSize = true;
+            this.lblTongNu.Location = new System.Drawing.Point(793, 369);
+            this.lblTongNu.Name = "lblTongNu";
+            this.lblTongNu.Size = new System.Drawing.Size(0, 13);
+            this.lblTongNu.TabIndex = 6;
+            // 
+            // MSSV
+            // 
+            this.MSSV.HeaderText = "MSSV";
+            this.MSSV.Name = "MSSV";
+            // 
+            // HoTen
+            // 
+            this.HoTen.HeaderText = "Họ Tên";
+            this.HoTen.Name = "HoTen";
+            // 
+            // GioiTinh
+            // 
+            this.GioiTinh.HeaderText = "Giới Tính";
+            this.GioiTinh.Name = "GioiTinh";
+            // 
+            // Khoa
+            // 
+            this.Khoa.HeaderText = "Khoa";
+            this.Khoa.Name = "Khoa";
+            // 
+            // DiemTB
+            // 
+            this.DiemTB.HeaderText = "Điểm TB";
+            this.DiemTB.Name = "DiemTB";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(873, 532);
+            this.Controls.Add(this.lblTongNu);
+            this.Controls.Add(this.lblTongNam);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtHoTen;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMaSV;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtDiemTB;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton radioNu;
+        private System.Windows.Forms.RadioButton radioNam;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox comboBoxKhoa;
+        private System.Windows.Forms.Label lblTongNam;
+        private System.Windows.Forms.Label lblTongNu;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MSSV;
+        private System.Windows.Forms.DataGridViewTextBoxColumn HoTen;
+        private System.Windows.Forms.DataGridViewTextBoxColumn GioiTinh;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Khoa;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DiemTB;
+    }
+}
+
---
+++ BTVNbuoi4/BTVNbuoi4/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BTVNbuoi4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            comboBoxKhoa.SelectedItem = "QTKD";   // Giá trị mặc định
+
+            // giới tính nữ được chọn mặc định
+            radioNu.Checked = true;
+
+            // --- Thêm dữ liệu sẵn ---
+            int rowIndex;
+
+            rowIndex = dataGridView1.Rows.Add();
+            DataGridViewRow row = dataGridView1.Rows[rowIndex];
+            row.Cells["MSSV"].Value = "SV001";
+            row.Cells["HoTen"].Value = "Nguyen Van A";
+            row.Cells["Khoa"].Value = "QTKD";
+            row.Cells["GioiTinh"].Value = "Nam";
+            row.Cells["DiemTB"].Value = 7.5;
+
+            rowIndex = dataGridView1.Rows.Add();
+            row = dataGridView1.Rows[rowIndex];
+            row.Cells["MSSV"].Value = "SV002";
+            row.Cells["HoTen"].Value = "Tran Thi B";
+            row.Cells["Khoa"].Value = "CNTT";
+            row.Cells["GioiTinh"].Value = "Nữ";
+            row.Cells["DiemTB"].Value = 8.2;
+
+            // Cập nhật tổng Nam/Nữ
+            CapNhatTongSV();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 1. Kiểm tra thông tin bắt buộc
+            if (string.IsNullOrWhiteSpace(txtMaSV.Text) ||
+                string.IsNullOrWhiteSpace(txtHoTen.Text) ||
+                string.IsNullOrWhiteSpace(txtDiemTB.Text))
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Kiểm tra điểm TB hợp lệ
+            if (!double.TryParse(txtDiemTB.Text, out double diemTB))
+            {
+                MessageBox.Show("Điểm trung bình phải là số!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+
+            // 2. Kiểm tra MSSV đã có trong DataGridView chưa
+            bool exists = false;
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (row.Cells["MSSV"].Value != null && row.Cells["MSSV"].Value.ToString() == txtMaSV.Text)
+                {
+                    exists = true;
+
+                    // Cập nhật dữ liệu
+                    row.Cells["HoTen"].Value = txtHoTen.Text;
+                    row.Cells["Khoa"].Value = comboBoxKhoa.SelectedItem.ToString();
+                    row.Cells["GioiTinh"].Value = radioNam.Checked ? "Nam" : "Nữ";
+                    row.Cells["DiemTB"].Value = diemTB;
+
+                    MessageBox.Show("Cập nhật dữ liệu thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    break;
+                }
+            }
+
+            // 3. Nếu chưa có MSSV -> thêm mới
+            if (!exists)
+            {
+                int rowIndex = dataGridView1.Rows.Add();
+                DataGridViewRow row = dataGridView1.Rows[rowIndex];
+
+                row.Cells["MSSV"].Value = txtMaSV.Text;
+                row.Cells["HoTen"].Value = txtHoTen.Text;
+                row.Cells["Khoa"].Value = comboBoxKhoa.SelectedItem.ToString();
+                row.Cells["GioiTinh"].Value = radioNam.Checked ? "Nam" : "Nữ";
+                row.Cells["DiemTB"].Value = diemTB;
+
+                MessageBox.Show("Thêm mới dữ liệu thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            // 4. Cập nhật tổng Nam/Nữ
+            CapNhatTongSV();
+
+            // 5. Reset form nếu cần
+            txtMaSV.Clear();
+            txtHoTen.Clear();
+            txtDiemTB.Clear();
+            comboBoxKhoa.SelectedIndex = 0;
+            radioNu.Checked = true;
+
+        }
+
+        private void txtMaSV_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string maSVCanXoa = txtMaSV.Text.Trim();
+
+            // 1. Kiểm tra nhập MSSV
+            if (string.IsNullOrEmpty(maSVCanXoa))
+            {
+                MessageBox.Show("Vui lòng nhập MSSV cần xóa!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // 2. Tìm MSSV trong DataGridView
+            DataGridViewRow rowCanXoa = null;
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (row.Cells["MSSV"].Value != null && row.Cells["MSSV"].Value.ToString() == maSVCanXoa)
+                {
+                    rowCanXoa = row;
+                    break;
+                }
+            }
+
+            // 3. Nếu không tìm thấy MSSV
+            if (rowCanXoa == null)
+            {
+                MessageBox.Show("Không tìm thấy MSSV cần xóa!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // 4. Hiển thị cảnh báo YES/NO
+            DialogResult result = MessageBox.Show($"Bạn có chắc muốn xóa sinh viên MSSV: {maSVCanXoa}?",
+                                                  "Xác nhận xóa", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                dataGridView1.Rows.Remove(rowCanXoa);
+                MessageBox.Show("Xóa sinh viên thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                // Reset TextBox nếu cần
+                txtMaSV.Clear();
+            }
+
+            CapNhatTongSV();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Kiểm tra người dùng không click vào header
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+
+                txtMaSV.Text = row.Cells["MSSV"].Value?.ToString();
+                txtHoTen.Text = row.Cells["HoTen"].Value?.ToString();
+                txtDiemTB.Text = row.Cells["DiemTB"].Value?.ToString();
+
+                comboBoxKhoa.SelectedItem = row.Cells["Khoa"].Value?.ToString();
+
+                string gioiTinh = row.Cells["GioiTinh"].Value?.ToString();
+                if (gioiTinh == "Nam")
+                    radioNam.Checked = true;
+                else
+                    radioNu.Checked = true;
+            }
+        }
+
+        private void CapNhatTongSV()
+        {
+            int tongNam = 0;
+            int tongNu = 0;
+
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (row.Cells["GioiTinh"].Value != null)
+                {
+                    string gt = row.Cells["GioiTinh"].Value.ToString();
+                    if (gt == "Nam")
+                        tongNam++;
+                    else if (gt == "Nữ")
+                        tongNu++;
+                }
+            }
+
+            lblTongNam.Text = tongNam.ToString();
+            lblTongNu.Text = tongNu.ToString();
+        }
+
+        private void dataGridView1_RowsAdded(object sender, DataGridViewRowsAddedEventArgs e)
+        {
+            CapNhatTongSV();
+        }
+
+        private void dataGridView1_RowsRemoved(object sender, DataGridViewRowsRemovedEventArgs e)
+        {
+            CapNhatTongSV();
+        }
+
+    }
+}
